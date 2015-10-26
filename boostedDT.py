6d30fc97fea83c2f160e2095ec082c7a5ff72b6f
Matt Schulman <matthewdschulman@gmail.com>
2015-10-26 11:10:14 -0400

    get it working...bug was with updating the weights

--- boostedDT.py
+++ boostedDT.py
@@ -5,8 +5,6 @@
 
 import numpy as np
 from sklearn import tree
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.metrics import accuracy_score
 
 class BoostedDT:
 
@@ -14,12 +12,13 @@ class BoostedDT:
         '''
         Constructor
         '''
-    	self.numBoostingIters = numBoostingIters
-	self.maxTreeDepth = maxTreeDepth
-	self.clf = [None] * numBoostingIters
-	self.numOfClasses = -1
-	self.betas = None
-	self.classes = None
+        self.numBoostingIters = numBoostingIters
+        self.maxTreeDepth = maxTreeDepth
+        self.clf = [None] * numBoostingIters
+        self.betas = None
+        self.numOfClasses = None
+        self.classes = None
+    
 
     def fit(self, X, y):
         '''
@@ -28,43 +27,43 @@ class BoostedDT:
             X is a n-by-d numpy array
             y is an n-dimensional numpy array
         '''
-	# update the num of classes and what those classes are
+        # update the num of classes and what those classes are
 	self.numOfClasses = np.unique(y).size 
 	self.classes = np.unique(y)
-	
-	n_size,d_size = X.shape
+		
+	n,d = X.shape
 
 	self.betas = np.zeros(self.numBoostingIters)
 
-	instance_weights = np.zeros(n_size)
-	#initialize each value of initial_weights to 1/n
-	for weightIndex in range(0, n_size):
-	    instance_weights[weightIndex] = 1.0/n_size
-
-	for iterNum in range(0, self.numBoostingIters):
-	    #train model h_t on X,y with instance weights w_t
-	    curDecisionTreeClassifier = DecisionTreeClassifier(max_depth = self.maxTreeDepth)
-	    self.clf[iterNum] = curDecisionTreeClassifier.fit(X, y, sample_weight=instance_weights)
-
-	    #compute the weighted training error rate of h_t
+	instance_weights = np.ones(n) / float(n)
+        
+        
+        # For T iterations
+        for iterNum in range(0, self.numBoostingIters):
+            #train model h_t on X,y with instance weights w_t
+            self.clf[iterNum] = tree.DecisionTreeClassifier(max_depth = self.maxTreeDepth)
+            self.clf[iterNum] = self.clf[iterNum].fit(X, y, sample_weight = instance_weights)
+          
+            #compute the weighted training error rate of h_t
 	    error = 0
 	    cur_pred = self.clf[iterNum].predict(X)
-	    for i in range(0,n_size):
+	    for i in range(0,n):
  		if y[i] != cur_pred[i]:
-		     error += instance_weights[i]
+	  	    error += instance_weights[i]
 
 	    #choose beta based on AdaBoost-SAMME equation
 	    cur_beta = 0.5*(np.log((1-error)/error) + np.log(self.numOfClasses - 1))
 	    self.betas[iterNum] = cur_beta
-
-	    #update all instance weights
-	    for i in range(0,n_size):
-	        instance_weights[i] = instance_weights[i] * np.exp(-1*cur_beta*y[i]*cur_pred[i])
-	
-	    #normalize w_t+1 to be a distribution
-	    normal_sum = np.sum(instance_weights)
-	    for i in range(0,n_size):
-		instance_weights[i] = instance_weights[i] / normal_sum
+          
+            # Update all instance Weights
+            if(iterNum < self.numBoostingIters - 1):
+                instance_weights = instance_weights * np.exp( -self.betas[iterNum] * (((y == cur_pred) * 2) - 1))
+            
+	        # Normalize the distribution
+                normal_sum = np.sum(instance_weights)
+	        for i in range(0,n):
+	            instance_weights[i] = instance_weights[i] / normal_sum
+          
 
     def predict(self, X):
         '''
@@ -74,13 +73,24 @@ class BoostedDT:
         Returns:
             an n-dimensional numpy array of the predictions
         '''
-	n,d = X.shape
-
+        # output predictions on the remaining data
+        n = X.shape[0]
         beta_matrix = np.zeros((n, self.numOfClasses))
-	    
+        
         for i in range(0, self.numBoostingIters):
             y_pred = self.clf[i].predict(X)
             for k in range(0, self.numOfClasses):
-	        beta_matrix[:, k] = beta_matrix[:, k] + (y_pred == self.classes[k]) * self.betas[i]
-	      
+                beta_matrix[:, k] = beta_matrix[:, k] + (y_pred == self.classes[k]) * self.betas[i]
+          
         return self.classes[np.argmax(beta_matrix, axis = 1)]
+        
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
