6ed8d7d218ad6e37fe2a74871fa76cc51586fb9f
Matt Schulman <matthewdschulman@gmail.com>
2015-10-26 09:48:02 -0400

    it seems right but there is some bug...idk what

--- boostedDT.py
+++ boostedDT.py
@@ -6,17 +6,20 @@
 import numpy as np
 from sklearn import tree
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
 
 class BoostedDT:
 
-    def __init__(self, numBoostingIters=100, maxTreeDepth=3, numOfClasses=9):
+    def __init__(self, numBoostingIters=100, maxTreeDepth=3):
         '''
         Constructor
         '''
     	self.numBoostingIters = numBoostingIters
 	self.maxTreeDepth = maxTreeDepth
-	self.clf = None
-	self.numOfClasses = numOfClasses
+	self.clf = [None] * numBoostingIters
+	self.numOfClasses = -1
+	self.betas = None
+	self.classes = None
 
     def fit(self, X, y):
         '''
@@ -25,33 +28,43 @@ class BoostedDT:
             X is a n-by-d numpy array
             y is an n-dimensional numpy array
         '''
-	self.clf = DecisionTreeClassifier(max_depth = self.maxTreeDepth)
-	n,d = X.shape
-	instance_weights = np.zeros(n)
-	#initialize each value of initial_weights to 1/n
-	for weightIndex in range(0, n):
-	    instance_weights[weightIndex] = 1.0/n
+	# update the num of classes and what those classes are
+	self.numOfClasses = np.unique(y).size 
+	self.classes = np.unique(y)
+	
+	n_size,d_size = X.shape
 
-	print instance_weights
+	self.betas = np.zeros(self.numBoostingIters)
+
+	instance_weights = np.zeros(n_size)
+	#initialize each value of initial_weights to 1/n
+	for weightIndex in range(0, n_size):
+	    instance_weights[weightIndex] = 1.0/n_size
+
 	for iterNum in range(0, self.numBoostingIters):
 	    #train model h_t on X,y with instance weights w_t
-	    self.clf.fit(X, y, sample_weight=instance_weights)
+	    curDecisionTreeClassifier = DecisionTreeClassifier(max_depth = self.maxTreeDepth)
+	    self.clf[iterNum] = curDecisionTreeClassifier.fit(X, y, sample_weight=instance_weights)
 
 	    #compute the weighted training error rate of h_t
 	    error = 0
-	    cur_pred = self.clf.predict(X)
-	    for i in range(0,n):
+	    cur_pred = self.clf[iterNum].predict(X)
+	    for i in range(0,n_size):
  		if y[i] != cur_pred[i]:
 		     error += instance_weights[i]
 
 	    #choose beta based on AdaBoost-SAMME equation
 	    cur_beta = 0.5*(np.log((1-error)/error) + np.log(self.numOfClasses - 1))
+	    self.betas[iterNum] = cur_beta
 
 	    #update all instance weights
-
+	    for i in range(0,n_size):
+	        instance_weights[i] = instance_weights[i] * np.exp(-1*cur_beta*y[i]*cur_pred[i])
+	
 	    #normalize w_t+1 to be a distribution
-
-	# set H(x) to the hypothesis
+	    normal_sum = np.sum(instance_weights)
+	    for i in range(0,n_size):
+		instance_weights[i] = instance_weights[i] / normal_sum
 
     def predict(self, X):
         '''
@@ -61,4 +74,13 @@ class BoostedDT:
         Returns:
             an n-dimensional numpy array of the predictions
         '''
-	return self.clf.predict(X)
+	n,d = X.shape
+
+        beta_matrix = np.zeros((n, self.numOfClasses))
+	    
+        for i in range(0, self.numBoostingIters):
+            y_pred = self.clf[i].predict(X)
+            for k in range(0, self.numOfClasses):
+	        beta_matrix[:, k] = beta_matrix[:, k] + (y_pred == self.classes[k]) * self.betas[i]
+	      
+        return self.classes[np.argmax(beta_matrix, axis = 1)]
