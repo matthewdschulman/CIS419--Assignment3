f4432ba4a820df6e093ff48f450a49c653ad433d
Matt Schulman <matthewdschulman@gmail.com>
2015-10-25 22:36:55 -0400

    set up framework for boosting algorithm

--- boostedDT.py
+++ boostedDT.py
@@ -24,12 +24,22 @@ class BoostedDT:
             X is a n-by-d numpy array
             y is an n-dimensional numpy array
         '''
-	#self.clf = DecisionTreeClassifier(max_depth = self.maxTreeDepth)
-	#self.clf.fit(X, y)
+	self.clf = DecisionTreeClassifier(max_depth = self.maxTreeDepth)
 	n,d = X.shape
-	instance_weights = np.zeros(d)
+	instance_weights = np.ones(n)
 	for iterNum in range(0, self.numBoostingIters):
 	    #train model h_t on X,y with instance weights w_t
+	    self.clf.fit(X, y, sample_weight=instance_weights)
+
+	    #compute the weighted training error rate of h_t
+
+	    #choose beta based on AdaBoost-SAMME equation
+
+	    #update all instance weights
+
+	    #normalize w_t+1 to be a distribution
+
+	# set H(x) to the hypothesis
 
     def predict(self, X):
         '''
